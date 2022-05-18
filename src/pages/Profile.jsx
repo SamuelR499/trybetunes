04207216a0dcd883ae7c401ed85eaa66c9a19283
Samuelr499 <samuelribeiro2@hotmail.com>
2022-05-18 16:24:30 -0300

    Requisito 1

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Profiles extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-profile" />
+    );
+  }
+}
---
+++ src/pages/ProfileEdit.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class ProfilesEdit extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit" />
+    );
+  }
+}
