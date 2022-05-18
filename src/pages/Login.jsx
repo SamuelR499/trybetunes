0222ff76857b43589e733787d4df7f3241d3081c
Samuelr499 <samuelribeiro2@hotmail.com>
2022-05-18 19:03:34 -0300

    requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,69 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
 
 export default class Login extends React.Component {
-  render() {
-    return (
-      <div data-testid="page-login" />
-    );
+  state = {
+    inputName: '',
+    ButtonDisable: true,
+    loading: false,
   }
+
+  onIputChange = ({ target }) => {
+    const { name } = target;
+    const valor = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: valor,
+    });
+    this.setState(({ inputName }) => {
+      const minLength = 3;
+      if (inputName.length >= minLength) {
+        return this.setState({ ButtonDisable: false });
+      }
+      return this.setState({ ButtonDisable: true });
+    });
+  }
+
+onButtonClick = async () => {
+  const { inputName } = this.state;
+  const { history } = this.props;
+  this.setState({ loading: true });
+  await createUser({ name: inputName });
+  history.push('search');
 }
+
+render() {
+  const { ButtonDisable, inputName, loading } = this.state;
+  return (
+    <div data-testid="page-login">
+      {loading ? <p>Carregando...</p> : (
+        <form action="">
+          <label htmlFor="login-name-input">
+            Name
+            <input
+              type="text"
+              id="login-name-input"
+              data-testid="login-name-input"
+              name="inputName"
+              value={ inputName }
+              onChange={ this.onIputChange }
+            />
+            <button
+              type="submit"
+              data-testid="login-submit-button"
+              disabled={ ButtonDisable }
+              onClick={ this.onButtonClick }
+            >
+              Entrar
+            </button>
+          </label>
+        </form>
+      ) }
+
+    </div>
+  );
+}
+}
+Login.propTypes = {
+  history: PropTypes.string.isRequired,
+};
