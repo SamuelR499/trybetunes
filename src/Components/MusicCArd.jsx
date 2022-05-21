f30e733868a2a551f26613aac602a83df14fc49e
Samuelr499 <samuelribeiro2@hotmail.com>
2022-05-21 12:13:42 -0300

    requisito 8

--- src/Components/MusicCArd.jsx
+++ src/Components/MusicCArd.jsx
@@ -1,25 +1,64 @@
 import React from 'react';
 import { Component } from 'react/cjs/react.production.min';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import getMusics from '../services/musicsAPI';
 
 export default class MusicCard extends Component {
-  render() {
-    const { music: { trackName, previewUrl } } = this.props;
-    return (
+  state = {
+    favorit: false,
+    isLoading: false,
+  }
 
-      <div>
-        <audio data-testid="audio-component" src={ previewUrl } controls>
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
-          {' '}
-          <code>audio</code>
-          .
-        </audio>
-        <h1>{trackName}</h1>
-      </div>
+  handleclick = async ({ target: { checked } }) => {
+    const { music: { trackId } } = this.props;
+    this.setState({
+      favorit: checked,
+      isLoading: true,
+    });
+    if (checked) {
+      await addSong(await getMusics(trackId));
+    }
+    this.setState({
+      isLoading: false,
+    });
+  }
+
+  render() {
+    const { music: { trackName, previewUrl, trackId } } = this.props;
+    const { favorit, isLoading } = this.state;
+    return (
+      isLoading
+        ? <p>Carregando...</p>
+        : (
+          <div>
+            <audio data-testid="audio-component" src={ previewUrl } controls>
+              <track kind="captions" />
+              O seu navegador não suporta o elemento
+              {' '}
+              <code>audio</code>
+              .
+            </audio>
+            <h1>{trackName}</h1>
+            <label htmlFor="checkbox">
+              Favorita
+              <input
+                name="checkbox"
+                type="checkbox"
+                data-testid={ `checkbox-music-${trackId}` }
+                onChange={ this.handleclick }
+                checked={ favorit }
+              />
+            </label>
+          </div>
+        )
     );
   }
 }
 MusicCard.propTypes = {
-  music: PropTypes.string.isRequired,
+  music: PropTypes.shape({
+    trackId: PropTypes.string,
+    trackName: PropTypes.string,
+    previewUrl: PropTypes.string,
+  }).isRequired,
 };
